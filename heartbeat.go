a134f9d208f25107ffec5f00174fc2f3d323024f
arthur <arthurkiller21@gmail.com>
2019-10-26 23:34:29 +0800

    implement pacemaker

--- heartbeat.go
+++ heartbeat.go
@@ -1,26 +1,61 @@
 // package sdk defined spanner sdk for tikv transection
 package sdk
 
-import "time"
+import (
+	"context"
+	"sync"
+	"time"
+
+	"go.uber.org/zap"
+)
+
+var (
+	transectionHeartBeatInterval = 1 // 1 second  per heartbeat
+)
 
 // Pacemaker sent heartbeat to every perticapate nodes
 type Pacemaker struct {
-	cli      string
-	kvnodes  []*tikvClient
-	interval time.Duration
-	ctx      chan int
+	txnid   uint64
+	kvnodes []*tikvClient
+	tick    <-chan time.Time
+	ctx     context.Context
+
+	sync.RWMutex
 }
 
-// NewPacemaker gen a pacemaker client
-func NewPacemaker(pdcli string) *Pacemaker {
-	return &Pacemaker{}
+// NewPacemaker gen a pacemaker client with context, txnid
+func NewPacemaker(c context.Context, txnid uint64) *Pacemaker {
+	lg.Debug("new pacemaker with txnid", zap.Uint64("txnid", txnid))
+	return &Pacemaker{
+		ctx:     c,
+		kvnodes: make([]*tikvClient, 0, 0xF),
+		tick:    time.Tick(time.Duration(transectionHeartBeatInterval) * time.Second),
+	}
 }
-
-// close stop heat the node
-func (p *Pacemaker) close() {}
 
 // AddNode add new node
-func (p *Pacemaker) AddNode() {}
+func (p *Pacemaker) AddNode(cli *tikvClient) {
+	lg.Debug("add tikv client to txn", zap.Uint64("txnid", p.txnid))
+	p.Lock()
+	p.kvnodes = append(p.kvnodes, cli)
+	p.Unlock()
+}
 
 // do heartbeat
-func (p *Pacemaker) heartbeat() {}
+func (p *Pacemaker) heartbeat() {
+	for {
+		select {
+		case <-p.ctx.Done():
+			lg.Debug("cancel heartbeat goroutine", zap.Uint64("txnid", p.txnid))
+			return
+		case <-p.tick:
+			p.Lock()
+			for i, _ := range p.kvnodes {
+				// ttl shoud larger than hb interval, currently 2 times
+				lg.Debug("do heartbeat", zap.Uint64("txnid", p.txnid))
+				p.kvnodes[i].HeartBeat(p.ctx, p.txnid, uint64(2*transectionHeartBeatInterval))
+			}
+			p.Unlock()
+		}
+	}
+}
